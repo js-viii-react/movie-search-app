9d851d587932c8b74d72b34a2ef90ab2d6722cae
Nebojsa Markovic <nebojsa91markovic@gmail.com>
2022-02-21 22:08:22 +0100

    added links on MovieListItem to redirect to MovieDetails

--- src/components/Movie/MovieDetails.js
+++ src/components/Movie/MovieDetails.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import API from "../../config/api";
+import { apiKey } from "../../config/apiKey";
 
 const MovieDetails = () => {
+  const [movie, setMovie] = useState({});
   //  AXIOS URL CALL
   // http://www.omdbapi.com/?i=${movieId}&${apiKey}
   // http://www.omdbapi.com/?i=tt0372784&apikey=APIKEY
 
-  return <div>Movie Details</div>;
+  const { movieId } = useParams();
+  console.log(movieId);
+
+  useEffect(() => {
+    API.get(`?i=${movieId}&${apiKey}`).then((res) => {
+      console.log(res);
+      setMovie(res.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>Movie Details</h1>
+      <p>Director: {movie.Director}</p>
+      <img src={movie.Poster} alt="" />
+    </div>
+  );
 };
 
 export default MovieDetails;
--- src/components/Movie/MoviesListItem.js
+++ src/components/Movie/MoviesListItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NO_IMAGE_FOUND = "https://i.ytimg.com/vi/np4n2DIOKVM/maxresdefault.jpg";
 
@@ -6,10 +7,10 @@ const MoviesListItem = ({ movie }) => {
   const poster = movie.Poster === "N/A" ? NO_IMAGE_FOUND : movie.Poster;
 
   return (
-    <div>
+    <Link to={`/movieDetails/${movie.imdbID}`}>
       <h2>{movie.Title}</h2>
       <img width="200" src={poster} alt={`The movie title: ${movie.Title}`} />
-    </div>
+    </Link>
   );
 };
 
